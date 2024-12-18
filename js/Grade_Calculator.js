4e01aa4cf43e316ca80137e8b02b055fdcb9cc2b
masumbi11 <masumbillah29060011musa@gmail.com>
2024-12-19 03:25:22 +0600

    Grade_Calculator

---
+++ js/Grade_Calculator.js
@@ -0,0 +1,19 @@
+const total = 100;
+if( total >=90 && total <= 100){
+    console.log('A')
+}
+else if( total >=80 && total <90){
+    console.log('B')
+}
+else if(total >=70 && total <80){
+    console.log('C')
+}
+else if(total >=60 && total <70){
+    console.log('D')
+}
+else if( total >=0 && total <60){
+    console.log('F')
+}
+else{
+    console.log('H')
+}

de17373979efc3a1c74b0be967c4c92f30b992fe
masumbi11 <masumbillah29060011musa@gmail.com>
2024-12-20 02:51:31 +0600

    Declare an array

---
+++ js/array alements/Declare_an_array.js
@@ -0,0 +1,13 @@
+let numbars=['amm', 'kathal', 'kla', 'appel', 'comla']
+console.log(numbars);
+
+ numbars.push('licu')
+console.log (numbars);
+numbars.push('gap', 'khajur')
+console.log(numbars);
+numbars.pop()
+console.log(numbars);
+
+
+
+

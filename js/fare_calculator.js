f8302ac8b6a300490f06cc66c3e81ec7c5e2a2bb
masumbi11 <masumbillah29060011musa@gmail.com>
2024-12-20 01:39:36 +0600

    fare_calculator

--- js/fare_calculator.js
+++ js/fare_calculator.js
@@ -1,13 +1,14 @@
 let regularPrise =800;
 
- let age =   60;
+ let age = [40];
 if (age <12){
     console.log('free')
 }
-// else if (students){
-// let students= regularPrise*50/100;
-//     console.log( regularPrise-students)
-// }
+
+else if (age <=20){
+  student= regularPrise*50/100;
+    console.log( regularPrise-student)
+}
 else if (age>=60){
     let seniorCitizens =regularPrise*15/ 100;
     console.log(regularPrise-seniorCitizens )
